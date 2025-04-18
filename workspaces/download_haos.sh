b48463fdcc810e48a7082341dc2a32672ff48fff
刘坤 <liukunup@163.com>
2025-04-18 06:15:52 +0000

    对下载后缀进行参数化

--- workspaces/download_haos.sh
+++ workspaces/download_haos.sh
@@ -5,4 +5,4 @@
 HAOS_VERSION=15.2
 HAOS_EXT=qcow2.xz # qcow2.xz / ova / vdi.zip
 
-wget https://github.com/home-assistant/operating-system/releases/download/${HAOS_VERSION}/haos_ova-${HAOS_VERSION}.qcow2.xz
+wget https://github.com/home-assistant/operating-system/releases/download/${HAOS_VERSION}/haos_ova-${HAOS_VERSION}.${HAOS_EXT}
