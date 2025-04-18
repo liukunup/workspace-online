9ac15e31d677df9fa3fe484cbc9c0c42b88d6e64
刘坤 <liukunup@163.com>
2025-04-18 04:13:23 +0000

    新增HAOS镜像下载脚本

---
+++ workspaces/download_haos.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# author: 我的代码温柔如风
+# date: 2025-04-18 12:09:00
+
+HAOS_VERSION=15.2
+HAOS_EXT=qcow2.xz # qcow2.xz / ova / vdi.zip
+
+wget https://github.com/home-assistant/operating-system/releases/download/${HAOS_VERSION}/haos_ova-${HAOS_VERSION}.qcow2.xz
